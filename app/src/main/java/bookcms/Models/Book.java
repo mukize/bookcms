d73c6c561473ab6d07946171c0ade0e2dccf1b57
Mukize <patrickmukize@gmail.com>
2023-10-16 13:25:14 +0200

    fix: Book model

--- app/src/main/java/bookcms/Models/Book.java
+++ app/src/main/java/bookcms/Models/Book.java
@@ -1,14 +1,52 @@
 package bookcms.Models;
 
-import java.sql.Date;
-
 public class Book {
 
-  Long id;
-  String title;
-  String description;
-  double price;
-  String image;
-  Date createdAt;
+  private Long id;
+  private String title;
+  private String description;
+  private double price;
+  private String image;
+  private String createdAt;
+
+  public Long getId() {
+    return id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public String getImage() {
+    return image;
+  }
+
+  public void setImage(String image) {
+    this.image = image;
+  }
+
+  public double getPrice() {
+    return price;
+  }
+
+  public void setPrice(double price) {
+    this.price = price;
+  }
+
+  public String getCreatedAt() {
+    return createdAt;
+  }
 
 }
